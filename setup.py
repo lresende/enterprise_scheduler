dc477f4ae30eb25e633393141a98143274735229
Luciano Resende <lresende@apache.org>
2018-07-10 17:56:17 -0400

    Update version (new dev release published to pypi)

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/lresende/enterprise_scheduler',
-    version='0.1.0.dev0',
+    version='0.1.0.dev1',
     zip_safe=False,
 )
