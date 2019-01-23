80259b9c0e3e734f6a070aaf8d73252d0418f690
Luciano Resende <lresende@apache.org>
2019-01-22 17:16:31 -0800

    Update startup script to use latest dependency releases/pre-release

--- enterprise_scheduler/resources/ffdl/start.sh
+++ enterprise_scheduler/resources/ffdl/start.sh
@@ -4,7 +4,12 @@ set -e
 
 pip install --upgrade pip
 pip install --upgrade ipykernel
-pip install --upgrade jupyter_enterprise_gateway-2.0.0.dev0-py2.py3-none-any.whl
+pip install --upgrade websocket-client
+pip install --pre --upgrade jupyter_enterprise_gateway
+pip install --upgrade yarn-api-client
+
+echo "Available Jupyter kernels..."
+jupyter kernelspec list
 
 jupyter enterprisegateway --ip=0.0.0.0 --port=8888 --NotebookApp.allow_remote_access=True & echo $! > enterprise_gateway.pid
 
