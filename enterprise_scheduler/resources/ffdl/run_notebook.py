83164da54be227eb86a677e2c9882041f0a5a69c
Luciano Resende <lresende@apache.org>
2019-01-25 14:56:41 -0800

    Use papermill for notebook execution

--- enterprise_scheduler/resources/ffdl/run_notebook.py
+++ enterprise_scheduler/resources/ffdl/run_notebook.py
@@ -1,55 +1,31 @@
 #!/usr/bin/env python
 
+import os
 import time
 import nbformat
 
-from enterprise_gateway.client.gateway_client import GatewayClient
+import papermill as pm
 
-print('')
-print('Waiting Enterprise Gateway to load...')
-
-time.sleep(10)
 
 print('')
 print('Start notebook execution...')
 
 try:
-    # start notebook
-    print('reading notebook contents')
-    notebook = None
-    with open('notebook.ipynb', 'r') as f:
-        notebook = nbformat.reads(f.read(), as_version=4)
+    input = 'notebook.ipynb'
+    output = os.environ['RESULT_DIR'] + '/result.ipynb'
 
-    if not notebook:
-        raise RuntimeError('Error reading notebook file')
-
-    print('Starting kernel...')
-
-    launcher = GatewayClient('localhost:8888')
-    kernel = launcher.start_kernel(notebook['metadata']['kernelspec']['name'])
-
-    if not kernel:
-        raise RuntimeError('Error starting kernel with kernelspec: {}'.format())
+    pm.execute_notebook(
+        input,
+        output
+    )
 
     time.sleep(10)
 
-    print('Starting cell execution')
-    for cell in notebook.cells:
-        print('Executing cell\n{}'.format(cell.source))
-        response = kernel.execute(cell.source)
-        print('Response\n{}'.format(response))
-        outputs = []
-        outputs.append(response)
-        cell['outputs'] = outputs
-
+    with open(output, 'r') as file:
+        print(file.read())
 
 except BaseException as base:
     print('Error executing notebook cells: {}'.format(base))
-
-finally:
-    print('Starting kernel shutdown')
-    # shutdown kernel
-    launcher.shutdown_kernel(kernel)
 
 print('Notebook execution done')
 print('')
--- enterprise_scheduler/resources/ffdl/start.sh
+++ enterprise_scheduler/resources/ffdl/start.sh
@@ -4,15 +4,9 @@ set -e
 
 pip install --upgrade pip
 pip install --upgrade ipykernel
-pip install --upgrade websocket-client
-pip install --pre --upgrade jupyter_enterprise_gateway
-pip install --upgrade yarn-api-client
+pip install --upgrade papermill
 
 echo "Available Jupyter kernels..."
 jupyter kernelspec list
 
-jupyter enterprisegateway --ip=0.0.0.0 --port=8888 --NotebookApp.allow_remote_access=True & echo $! > enterprise_gateway.pid
-
 ./run_notebook.py
-
-pkill -F enterprise_gateway.pid
